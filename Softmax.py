e3baeb0930689021651e3f49879d8287ebf785cc
le0nard01 <68928875+le0nard01@users.noreply.github.com>
2021-03-18 18:31:36 -0300

    Update Softmax.py

--- Softmax.py
+++ Softmax.py
@@ -8,6 +8,7 @@ class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs,n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
